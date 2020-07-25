fa19fd5e9a5554b7352f4fbaca20ea764d9e2cdc
Ｈｉｔｓｕｋａ（芋ひュ） <61979182+No-OnE-Kn0wS-Me@users.noreply.github.com>
2020-07-25 10:19:55 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -34,5 +34,5 @@ if __name__ == "__main__" :
         api_hash=Config.API_HASH,
         plugins=plugins
     )
-    Config.AUTH_USERS.add(683538773)
+    Config.AUTH_USERS.add(861055237)
     app.run()
