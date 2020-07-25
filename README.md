fac4d42e80e0bbe830fab79b4e048faf7c2f6afd
Ｈｉｔｓｕｋａ（芋ひュ） <61979182+No-OnE-Kn0wS-Me@users.noreply.github.com>
2020-07-25 09:46:14 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-# FileRenameBot
+## Rename Bot 
+---
+
+An Open Source Rename Telegram RoBot
+
+**My Feature**:
+
+👉 Rename any Telegram Files with Permanent Thumbnail Support
+
+
+### Installation
+
+
+### You can also tap the Deploy To Heroku button below to deploy straight to Heroku!
+[![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy?template=https://github.com/Aid-3n/Rename-TG-BOT)
+
+
+#### The Hard Way
+***NOT AVAILABLE Coz It's HARD😜😂***
+
+## Credits, and Thanks Always Go It's Beloved Developers :
+
+* [SpEcHlDe](https://telegram.dog/SpEcHlDe) 
+* [Dan Tès](https://telegram.dog/haskell) 
+* [Yoily](https://telegram.dog/YoilyL)
+* [Anand](https://telegram.dog/Anandpskerala)
+
+- For FeedBack and Suggestions Contact [Aiden](https://telegram.dog/Aid_3n)
+
+#### LICENSE
+- GPLv3
+
---
+++ translation.py
@@ -0,0 +1,30 @@
+class Translation(object):
+    START_TEXT = """ Simple Telegram Renaming BOT Edited By This <a href="https://tx.me/AID_3N">MAN</a>
+              🎈 @DX_BOTZ 🎈
+<b>Please send me any Telegram file and reply /rename New Name.extn</b>
+SEE /help For My Features"""
+
+    RENAME_403_ERR = "There Is Some Error With The File"
+    ABS_TEXT = "What Are You Trying To Do,Mate?"
+    UPGRADE_TEXT = "CONTACT @AID_3N"
+    DOWNLOAD_START = "Give Me Some Time Niggah(〒﹏〒)"
+    UPLOAD_START = "Yay, Completed Downloading(;ŏ﹏ŏ)"
+    RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nDetected File Size: {}\nSorry WTF Do You Think! I'll Upload It? use @PUBLIC_LEECH_2"
+    AFTER_SUCCESSFUL_UPLOAD_MSG = "**Thank you for Using Me,For Downloading Files Join @PUBLIC_LEECH_2**"
+    AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded in {} seconds.\nUploaded in {} seconds Use @PUBLIC_LEECH_2 To Download From Links"
+    NOT_AUTH_USER_TEXT = "CONTACT @AID_3N"
+    NOT_AUTH_USER_TEXT_FILE_SIZE = "IF You Got This Message You Should Contact @AID_3N"
+    SAVED_CUSTOM_THUMB_NAIL = "Custom File thumbnail Saved ✅ This Is Permanent Thumbnail ᕙ(  • ‿ •  )ᕗ "
+    DEL_ETED_CUSTOM_THUMB_NAIL = "Custom thumbnail cleared succesfully🤦"
+    FF_MPEG_DEL_ETED_CUSTOM_MEDIA = "Media cleared succesfully."
+    SAVED_RECVD_DOC_FILE = "File Downloaded Successfully😎"
+    CUSTOM_CAPTION_UL_FILE = """@PUBLIC_LEECH_2"""
+    HELP_USER = """<b>How To Use Me</b>
+    1. Send me any Telegram File(Some Files Gets Error)
+    2. Reply to that message to /rename NEW NAME.EXTENSION
+    FEATURES
+    ➡Rename Telegram File With Custom Thumbnail.
+    ➡Convert Streamable File(Video) To File.
+    ➡ FOR Downloading Files At Highspeed Join @PUBLIC_LEECH_2"""
+    REPLY_TO_DOC_FOR_RENAME_FILE = "Reply The New Name With /rename Filename.extn"
+    IFLONG_FILE_NAME = """You Gotta Be Kidding Me...Decrease The Number Of Letters😆😉"""
