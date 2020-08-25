fe8824e01bb29ca8b919c5b6a0981ac0feb564bc
Ｈｉｔｓｕｋａ（芋ひュ） <61979182+No-OnE-Kn0wS-Me@users.noreply.github.com>
2020-08-25 13:28:56 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -36,5 +36,5 @@ class Config(object):
     DEF_WATER_MARK_FILE = ""
     # Database url
     DB_URI = os.environ.get("DATABASE_URL", "")
-    CHANNEL_ID = os.environ.get("CHANNEL_ID", -1001400306304)) 
-
+    CHANNEL_ID = os.environ.get("CHANNEL_ID", -1001400306304)
+   
