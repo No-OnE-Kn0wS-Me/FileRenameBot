24dee8600d2abab48ab3ac3497f08bcce0b84055
Ｈｉｔｓｕｋａ（芋ひュ） <61979182+No-OnE-Kn0wS-Me@users.noreply.github.com>
2020-08-29 22:30:34 +0530

    Update audio_converter.py

--- plugins/audio_converter.py
+++ plugins/audio_converter.py
@@ -102,7 +102,7 @@ def convert_to_audio(bot, update):
                 # https://pillow.readthedocs.io/en/3.1.x/reference/Image.html#create-thumbnails
             # try to upload file
             c_time = time.time()
-           await bot.send_audio(
+            bot.send_audio(
                 chat_id=update.chat.id,
                 audio=audio_file_location_path,
                 caption=description,
