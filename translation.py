5283aff7dd107089594d54272250e45c4a6d845d
KarthikStark9 <74292594+KarthikStark9@users.noreply.github.com>
2020-11-26 11:40:53 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,13 +1,13 @@
 class Translation(object):
-    START_TEXT = """ <b> Hi {} , I'm A Simple File Renamer+File To Video Converter Bot With Permanent Thumbnail support!💯</b> \n
+    START_TEXT = """ <b>Hi {} , I'm A File Renamer+File To Video Converter Bot With Permanent Thumbnail.</b> \n
 
 <b>Bot Maintained By: @KarthikStark</b> \n
-<b>Do /help for more...</b> \n
+<b>Send /help for more...</b> \n
 """
 
     RENAME_403_ERR = "What Are You Doing? Bruuh!!"
-    BANNED_USER_TEXT = "Sorry!! But My Owner As Banned You From Using This Service. That Means You Can't Run Meh Now! \n \n Contact : @No_OnE_Kn0wS_Me For more Details.. " 
-    ABS_TEXT = "What Are You Trying To Do,Mate?"
+    BANNED_USER_TEXT = "Sorry!! But you're not allowed to use this Service.. " 
+    ABS_TEXT = "What Are You Trying To Do?"
     UPGRADE_TEXT = "CONTACT @KarthikStark "
     DOWNLOAD_START = "<b>Downloading..</b>"
     UPLOAD_START = "<b>Uploading..</b>"
@@ -16,22 +16,22 @@ class Translation(object):
     AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded in {} seconds.\nUploaded in {} seconds"
     NOT_AUTH_USER_TEXT = "CONTACT @KarthikStark "
     NOT_AUTH_USER_TEXT_FILE_SIZE = "IF You Got This Message You Should Contact @KarthikStark "
-    SAVED_CUSTOM_THUMB_NAIL = "<b>Thumbnail Saved ✅ </b> /deletethumbnail ❤"
+    SAVED_CUSTOM_THUMB_NAIL = "<b>Thumbnail Saved ✅ </b>        /deletethumbnail "
     DEL_ETED_CUSTOM_THUMB_NAIL = "Thumbnail Cleared"
     FF_MPEG_DEL_ETED_CUSTOM_MEDIA = "Media cleared succesfully."
-    SAVED_RECVD_DOC_FILE = "<b>File Downloaded Successfully 😎</b>"
-    REPLY_TO_DOC_FOR_RENAME_FILE = "<b>Please Reply To An File With /rename fil name extension To rename a file</b>"
-    REPLY_TO_DOC_FOR_C2V = "<b> Please Reply To An File With /c2v To Convert It Into Streamable video File</b>"
-    REPLY_TO_DOC_FOR_C2A = "<b> Please Reply To An File/video With /c2a To Convert It Into Audio File</b>"
+    SAVED_RECVD_DOC_FILE = "<b>File Downloaded Successfully.</b>"
+    REPLY_TO_DOC_FOR_RENAME_FILE = "<b>Reply To A File With /rename file name .extension To rename a File.</b>"
+    REPLY_TO_DOC_FOR_C2V = "<b>Reply To A File With /c2v To Convert a File to Video.</b>"
+    REPLY_TO_DOC_FOR_C2A = "<b>Reply To A File/video With /c2a To Convert It Into Audio.</b>"
     CUSTOM_CAPTION_UL_FILE = " "
-    NO_CUSTOM_THUMB_NAIL_FOUND = "No Thumbnail found,SED🤒"
+    NO_CUSTOM_THUMB_NAIL_FOUND = "No Thumbnail found."
     USER_ADDED_TO_DB = "User <a href='tg://user?id={}'>{}</a> added to {} till {}."
     IFLONG_FILE_NAME = """WTF?...Decrease The Number Of Letters"""
-    ABOUT_ME = """<b>Meh : \n An Telegram File Renamer Bot Which Can Rename A Telegram Files. \n You Can Set Permanent Thumbnail For The File So You Don't Have To Send Custom Thumbs All The Time.\n \n i can also convert files into videos \n Join @Mai_BoTs If You Found Meh Useful</b>"""
-    HELP_USER = """Ohh You Want Help?? 😅
+    ABOUT_ME = """<b>Meh : \n A Telegram File Renamer + File to Video Converter Which Can Rename A Telegram Files with Thumbnail.</b>"""
+    HELP_USER = """Want Help??
     
 1.🔹 <b>Send me any Telegram File.</b> \n
 2.🔹 <b>Reply to that message with /rename New file name.extnsion</b> \n
-3.🔹 <b> Send A Photo to make it as permanent Thumbnail</b> \n
-4.🔹 <b> I Can Also Convert Files Into Video through /c2v Command </b>   \n \n <b>Made With Love By: @KarthikStark <b>
+3.🔹 <b>Send A Photo to make it as Thumbnail.</b> \n
+4.🔹 <b>I Can Also Convert Files Into Video through /c2v Command. <b>
 
