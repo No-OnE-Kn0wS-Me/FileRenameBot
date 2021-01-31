d192359de24b705cfb454b2bd7a10f69d4b939b7
Ｈｉｔｓｕｋａ（芋ひュ） <61979182+No-OnE-Kn0wS-Me@users.noreply.github.com>
2021-01-31 15:01:22 +0530

    Update script.py

--- script.py
+++ script.py
@@ -1,20 +1,22 @@
 class script(object):
     START_TEXT = """ A Simple File Renamer Bot With Permanent Thumbnail support!💯
 
-<b>Send me any Telegram file and choose appropriate option! </b>"""
+<b>Send me any Telegram file then select a option! </b>"""
 
     RENAME_403_ERR = "What Are You Doing? You are Banned"
-    UPGRADE_TEXT = "CONTACT @prgofficial"
-    DOWNLOAD_START = "Give Me Some Time..."
+    UPGRADE_TEXT = "CONTACT @No_OnE_Kn0wS_Me "
+    DOWNLOAD_START = "Trying to Download Please Wait..."
     UPLOAD_START = "Starting to upload..."
-    AFTER_SUCCESSFUL_UPLOAD_MSG = "**Thank you for Using Me > ©  @prgofficial **"
+    AFTER_SUCCESSFUL_UPLOAD_MSG = "**Thank you for Using Me > Do Support @Mai_BotS **"
     SAVED_THUMB = "Thumbnail Saved ✅ This Is Permanent"
     DEL_THUMB = "Thumbnail cleared succesfully!"
     NO_THUMB = "No thumbnails found!"
     SAVED_RECVD_DOC_FILE = "File Downloaded Successfully 😎"
     CUSTOM_CAPTION_UL_FILE = " "
-    HELP_USER = """It's not that complicated😅
+    HELP_USER = """ You Can Use meh Easily! 
     
 1. Send me any Telegram File.
-2. Choose appropriate option."""
+2. Choose a option from Below 
+
+(Note: You don't have to give extension like .mkv,.mp4)."""
 
