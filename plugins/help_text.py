3c1ee0442e73f27cdbbc44d58f9b479039efed48
Ｈｉｔｓｕｋａ（芋ひュ） <61979182+No-OnE-Kn0wS-Me@users.noreply.github.com>
2021-01-31 15:45:44 +0530

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -41,7 +41,7 @@ def help_user(bot, update):
 def send_start(bot, update):
     # logger.info(update)
     
-    await bot.send_message(
+     bot.send_message(
         chat_id=update.chat.id,
         text=script.START_TEXT.format(update.from_user.first_name),
         reply_markup=InlineKeyboardMarkup(
