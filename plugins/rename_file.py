ae02b03f0d37df8fe9f60f48ebc6bc18b63e3002
Ｈｉｔｓｕｋａ（芋ひュ） <61979182+No-OnE-Kn0wS-Me@users.noreply.github.com>
2021-01-31 15:27:12 +0530

    Update rename_file.py

--- plugins/rename_file.py
+++ plugins/rename_file.py
@@ -19,7 +19,7 @@ from script import script
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, ForceReply
 
-from plugins.helpers import progress_for_pyrogram
+from plugins.statics import progress_for_pyrogram
 
 from hachoir.metadata import extractMetadata
 from hachoir.parser import createParser
