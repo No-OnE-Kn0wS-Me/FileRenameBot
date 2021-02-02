1cb2a4ae527ea900852c293b6183d255c67d102e
Ｈｉｔｓｕｋａ（芋ひュ） <61979182+No-OnE-Kn0wS-Me@users.noreply.github.com>
2021-02-02 13:47:50 +0530

    Update chat_base.py

--- helper_funcs/chat_base.py
+++ helper_funcs/chat_base.py
@@ -19,7 +19,7 @@ else:
 # the Strings used for this "thing"
 from translation import Translation
 
-from pyrogram import Client, Filters
+from pyrogram import Client, filters 
 
 # the Telegram trackings
 from chatbase import Message
