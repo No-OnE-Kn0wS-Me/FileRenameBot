4bf127f17af8a19e11469298153c54f13829d79a
Ｈｉｔｓｕｋａ（芋ひュ） <61979182+No-OnE-Kn0wS-Me@users.noreply.github.com>
2021-02-02 13:30:47 +0530

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -7,6 +7,7 @@ import logging
 logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
 import os
 import sqlite3
