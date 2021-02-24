0da89b1d6e1121f13d2612b85e7ce3dbbdae0c65
Ｈｉｔｓｕｋａ（芋ひュ） <61979182+No-OnE-Kn0wS-Me@users.noreply.github.com>
2021-02-24 23:36:13 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -22,7 +22,7 @@ if __name__ == "__main__" :
         root="plugins"
     )
     app = pyrogram.Client(
-        "AnyDLBot",
+        "Zee5",
         bot_token=Config.TG_BOT_TOKEN,
         api_id=Config.APP_ID,
         api_hash=Config.API_HASH,
