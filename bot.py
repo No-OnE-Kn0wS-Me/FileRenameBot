f4d18c9024d046280d006a6d9ccfe6c66c64f0c0
Ｈｉｔｓｕｋａ（芋ひュ） <61979182+No-OnE-Kn0wS-Me@users.noreply.github.com>
2021-02-24 21:39:01 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,27 +1,21 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-# (c) Shrimadhav U K
-
-# the logging things
 import logging
 logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
 import os
 
-# the secret configuration specific things
 if bool(os.environ.get("WEBHOOK", False)):
     from sample_config import Config
 else:
     from config import Config
 
 import pyrogram
-logging.getLogger("pyrogram").setLevel(logging.WARNING)
+
 
 
 if __name__ == "__main__" :
-    # create download directory, if not exist
     if not os.path.isdir(Config.DOWNLOAD_LOCATION):
         os.makedirs(Config.DOWNLOAD_LOCATION)
     plugins = dict(
@@ -29,6 +23,7 @@ if __name__ == "__main__" :
     )
     app = pyrogram.Client(
         "AnyDLBot",
+        "Zee5",
         bot_token=Config.TG_BOT_TOKEN,
         api_id=Config.APP_ID,
         api_hash=Config.API_HASH,
