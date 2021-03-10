9150f25aac7fbe2fc38f7790e9f2ff5c03b08d7d
Ｈｉｔｓｕｋａ（芋ひュ） <61979182+No-OnE-Kn0wS-Me@users.noreply.github.com>
2021-03-10 19:48:38 +0530

    Update force.py

--- plugins/force.py
+++ plugins/force.py
@@ -26,7 +26,7 @@ from pyrogram.errors import UserNotParticipant, UserBannedInChannel
 
 
 @pyrogram.Client.on_message(pyrogram.filters.text)
-async def echo(bot, update):
+async def text(bot, update):
     if update.from_user.id in Config.BANNED_USERS:
         await update.reply_text("You are B A N N E D 🤣🤣🤣🤣")
         return
