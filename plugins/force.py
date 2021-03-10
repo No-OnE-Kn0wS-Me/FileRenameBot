077fcb20cdf279c314e010f9140321e46de662c6
Ｈｉｔｓｕｋａ（芋ひュ） <61979182+No-OnE-Kn0wS-Me@users.noreply.github.com>
2021-03-10 22:19:11 +0530

    Update force.py

--- plugins/force.py
+++ plugins/force.py
@@ -24,7 +24,7 @@ from pyrogram import Client
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
 from pyrogram.types import ReplyKeyboardMarkup, InlineKeyboardMarkup, InlineKeyboardButton
-from pyrogram.errors import UserNotParticipant, UserBannedInChannel
+from pyrogram.errors import UserNotParticipant, UserBannedInChannel, USER_ALREADY_PARTICIPANT 
 from helper_funcs.chat_base import TRChatBase
 
 
@@ -50,7 +50,7 @@ async def text(bot, update):
               ])
             )
             return
-        except Exception:
+        except USER_ALREADY_PARTICIPANT:
             await update.reply_text(Translation.START_TEXT.format(update.from_user.first_name),
         reply_markup=InlineKeyboardMarkup(
             [
