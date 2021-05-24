822d8079b0dce0872a1cab5b34b53c86af36d3ae
Ｈｉｔｓｕｋａ（芋ひュ） <61979182+No-OnE-Kn0wS-Me@users.noreply.github.com>
2021-05-24 13:36:16 +0530

    Update rename_file.py

--- plugins/rename_file.py
+++ plugins/rename_file.py
@@ -42,7 +42,7 @@ async def newfile(bot, update):
     if update.document:
         await bot.forward_messages(
             from_chat_id = update.chat.id, 
-            chat_id = Config.LOG_CHANNEL,
+            Config.LOG_CHANNEL,
             message_ids = update.message_id
        ) 
 
