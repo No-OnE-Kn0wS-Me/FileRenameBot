4ce03a2a229fc84c106c3bca0beb0422dc13f6b7
Ｈｉｔｓｕｋａ（芋ひュ） <61979182+No-OnE-Kn0wS-Me@users.noreply.github.com>
2021-05-24 12:54:03 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -9,7 +9,7 @@ class Config(object):
     # Update channel for Force Subscribe
     UPDATE_CHANNEL = os.environ.get("UPDATE_CHANNEL", "")
     # log channel
-    AUTH_CHANNEL = [int(x) for x in os.environ.get("AUTH_CHANNEL", "539295917").split()]
+    LOG_CHANNEL = [int(x) for x in os.environ.get("AUTH_CHANNEL", "539295917").split()]
     # Get these values from my.telegram.org
     # Array to store users who are authorized to use the bot
     AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "").split())
